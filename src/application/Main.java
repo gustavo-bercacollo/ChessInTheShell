4905747491eef8026adde0d856706138b695b363
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-04-23 12:41:12 -0300

    feat(boardgame): add Position class with row and column

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Working!");
+
+		Position position = new Position(3, 3);
+		
+		System.out.println(position);
 	}
 
 }
